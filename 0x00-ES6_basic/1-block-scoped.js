e199815233c35dbe8b7b25a08b5589fe18bd954a
oluwasmart <lebioluwasoji@gmail.com>
2023-04-17 15:48:04 -0700

    1 block

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,6 @@
-/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
     const task = true;
